92d1fcefe78d8aa5d9e0bd9fa09b3713bf1613bb
dubisdev <dubisdev@gmail.com>
2024-02-26 20:00:39 +0100

    docs: add documentation for the new API

--- packages/api/src/extensions/ExtensionContext.ts
+++ packages/api/src/extensions/ExtensionContext.ts
@@ -1,15 +1,52 @@
 import type { Item } from "./items"
 
+/**
+ * The context that is provided to the extension when it is executed.
+ * This context provides the extension with the input term, and a function that can be called to display the results of the extension.
+ * 
+ */
 export interface ExtensionContext {
+    /**
+     * A function that can be called to display the results of the extension.
+     */
     display: (items: Item[]) => Promise<void>
+    /**
+     * Hide result from results list by id.
+     * You can use it to remove temporar results, like "loading..." placeholder
+     */
     hide: (id: string) => void
+    /**
+     * The input term that the user has entered.
+     */
     term: string
     update: (id: string, item: Item) => void
+    /**
+     * Common actions that the extension can perform.
+     */
     actions: {
+        /**
+         * Copy the given text to the clipboard.
+         */
         copyToClipboard: (text: string) => Promise<void>,
+        /**
+         * Hide the Rokii window.
+         */
         hideWindow: () => Promise<void>,
+        /**
+         * Open the given URL in the default browser.
+         */
         open: (url: string) => Promise<void>,
+        /**
+         * Open the given path in the file explorer.
+         */
         reveal: (path: string) => Promise<void>,
+        /**
+         * Replace the current term with the given term.
+         */
         replaceTerm: (term: string) => void
-    }
+    },
+    /**
+     * An object containing the settings for the extension.
+     */
+    settings: Record<string, any>
 }
--- packages/api/src/extensions/ExtensionModule.ts
+++ packages/api/src/extensions/ExtensionModule.ts
@@ -2,11 +2,41 @@ import type { ExtensionContext } from "./ExtensionContext"
 
 export interface ExtensionModule {
     icon: string
+    /**
+     * The name of the extension.
+     * 
+     * This name will be displayed in the extension list, and will be used to identify the extension in the settings.
+     */
     name: string
+    /**
+     * The entry point for the extension. This function will be called to execute the extension.
+     */
     run: (context: ExtensionContext) => Promise<void>
+    /**
+     * The settings for the extension.
+     * 
+     * This is a WIP, and will be used to provide the extension with settings from the user.
+     */
     settings?: any
-    initialize?: (...args: any[]) => void
-    initializeAsync?: (...args: any[]) => Promise<void>
+    /**
+     * A function that will be called when the extension is initialized.
+     * It will be called synchronously, before the extension is executed.
+     * 
+     * For long running tasks, use `initializeAsync` instead.
+     * 
+     */
+    initialize?: (settings: Record<string, any>) => void
+    initializeAsync?: (callback: any, settings: Record<string, any>) => Promise<void>
+    /**
+     * This method will be called when the initializeAsync method calls the callback.
+     * This is useful to retrieve data for the extension, and to perform long running tasks.
+     */
     onMessage?: (data: unknown) => void
+    /**
+     * A list of apps that the extension provides.
+     * 
+     * Apps are a way to group multiple extension results together.
+     * They are shown in a separate page with a private results list.
+     */
     apps?: Record<string, (context: ExtensionContext) => Promise<void>>
 }
