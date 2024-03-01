72315d0bb838cfc021352d51fe66b57b8284cd2e
dubisdev <dubisdev@gmail.com>
2024-03-01 10:37:04 +0100

    chore: move autocomplete component to tailwind

--- app/src/main/components/InputBox/Autocomplete.tsx
+++ app/src/main/components/InputBox/Autocomplete.tsx
@@ -1,10 +1,12 @@
 import { useAutocomplete } from '@/main/hooks/useAutocomplete'
-import styles from './styles.module.css'
 
 export const Autocomplete = () => {
   const { value } = useAutocomplete()
 
   if (!value) return null
 
-  return <input tabIndex={-1} disabled className={styles.autocomplete} value={value} />
+  return <input
+    tabIndex={-1} disabled value={value}
+    className="absolute h-full bg-transparent border-none whitespace-pre text-[var(--secondary-font-color)]"
+  />
 }
