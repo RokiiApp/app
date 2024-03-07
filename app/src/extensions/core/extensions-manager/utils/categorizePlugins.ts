ddd668897c6c9b329c0be3c5d5793dae22601248
dubisdev <dubisdev@gmail.com>
2024-03-07 11:27:58 +0100

    chore: add native support for Object.groupBy

--- app/src/extensions/core/extensions-manager/utils/categorizePlugins.ts
+++ app/src/extensions/core/extensions-manager/utils/categorizePlugins.ts
@@ -1,19 +1,17 @@
 import { PluginInfo } from "../types";
 
 export const categorizePlugins = (plugins: PluginInfo[]) => {
-    // ref https://github.com/microsoft/TypeScript/issues/47171
-    // @ts-ignore
     const grouped = Object.groupBy(plugins, (plugin: PluginInfo) => {
         if (plugin.isDebugging) return '🐛 Debugging';
         if (plugin.isUpdateAvailable) return '🆕 Updates';
         if (plugin.isInstalled) return '💫 Installed';
         if (plugin.name) return '🔎 Available';
-    })
+        return '🔌 Other';
+    });
 
     const result: (PluginInfo | string)[] = [];
 
     Object.entries(grouped).forEach(([category, plugins]) => {
-        // @ts-ignore
         result.push(category, ...plugins);
     })
 
