7784d16000e3f87dbfd9787f9de50510703b9770
dubisdev <dubisdev@gmail.com>
2024-03-08 10:09:41 +0100

    feat: Add option to keep term on AppItems

--- packages/api/src/extensions/items/AppItem.ts
+++ packages/api/src/extensions/items/AppItem.ts
@@ -4,13 +4,16 @@ import { Item, type ItemParams } from "./Item"
 export class AppItem extends Item {
     readonly type = ItemTypes.APP
     appName: string
+    keepTerm: boolean
 
     constructor(action: AppItemParams) {
         super(action)
         this.appName = action.appName
+        this.keepTerm = action.keepTerm || false
     }
 }
 
 export type AppItemParams = ItemParams & {
-    appName: string
+    appName: string,
+    keepTerm?: boolean
 }
