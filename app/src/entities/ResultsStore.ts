e30fd03eaacd2608b5e3b4f380ec59929c054319
dubisdev <dubisdev@gmail.com>
2024-03-09 16:34:31 +0100

    chore: extract ActionsMap type

--- app/src/entities/ResultsStore.ts
+++ app/src/entities/ResultsStore.ts
@@ -2,12 +2,14 @@ import type { Item } from '@rokii/api'
 import type { Result } from '@/entities/result/Result'
 import { StoreApi, UseBoundStore } from 'zustand'
 
+export type ActionsMap = Map<string, Result>
+
 /**
  * A Store that handles the available Results
  * to be displayed in the app
  */
 export interface ResultsStore {
-    actionsMap: Map<string, Result>
+    actionsMap: ActionsMap
     actions: Result[]
   
     addActions: (actions: Item[], extensionName: string) => void
