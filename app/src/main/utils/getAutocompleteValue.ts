33a016396558a097023f392dfcb61b3b4c1b1917
dubisdev <dubisdev@gmail.com>
2024-03-12 14:24:14 +0100

    docs: add info

--- app/src/main/utils/getAutocompleteValue.ts
+++ app/src/main/utils/getAutocompleteValue.ts
@@ -1,5 +1,15 @@
 import escapeStringRegexp from 'escape-string-regexp'
 
+/**
+ * Calculates if the autocomplete suggestion matches the term and returns the value to be displayed
+ * 
+ * - If the term is "re" and the suggestion is "react", the value to be displayed is "react"
+ * - If the term is "re" and the suggestion is "React", the value to be displayed is "react"
+ * (we respect the case of the term to avoid overlapping the suggestion UI with the input component)
+ * - If the term is "act" and the suggestion is "react", the value should not be displayed
+ * as it does not match the term
+ * 
+ */
 export const getAutocompleteValue = (
   autocompleteSuggestion: string,
   term: string
--- app/src/services/getReadme.ts
+++ app/src/services/getReadme.ts
@@ -1,6 +1,7 @@
 import { decode } from 'js-base64'
 /**
  * Get plugin Readme.md content
+ * @param repo The full name of the repo: `owner/repo`
  */
 export const getReadme = async (repo: string) => {
     const response = await fetch(`https://api.github.com/repos/${repo}/readme`)
