3455fc0864b60cf377332604b4c12f08961c236e
dubisdev <dubisdev@gmail.com>
2024-03-14 10:43:44 +0100

    chore: migrate commands to shell api

--- app/src-tauri/src/services/get_installed_apps.rs
+++ app/src-tauri/src/services/get_installed_apps.rs
@@ -1,14 +1,14 @@
-use tauri::api::process::Command;
+use tauri::AppHandle;
+use tauri_plugin_shell::ShellExt;
 
 // TODO - Migrate to this PWsh script
 // https://github.com/oliverschwendener/ueli/blob/main/src/main/Extensions/ApplicationSearch/Windows/usePowershellScripts.ts
 #[tauri::command(async)]
-pub async fn get_installed_apps() -> String {
-    let result = Command::new("powershell.exe")
+pub async fn get_installed_apps(app: AppHandle) -> String {
+    let result = app.shell().command("powershell.exe")
         .args(["$startApps = Get-StartApps; foreach($startApp in $startApps) { echo $startApp.Name; echo $startApp.AppId }"])
         .output()
-        .expect("failed to execute process");
+        .await.unwrap();
 
-
-    return result.stdout;
+    return String::from_utf8(result.stdout).expect("error");
 }
